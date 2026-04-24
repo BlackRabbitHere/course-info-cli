8bb33497660490f4d750e4a82a5020a89dd22d46
Kshitij <kshitijkumar514@gmail.com>
2026-04-24 10:18:27 +0530

    feat: initialize app and integrate HTTP client for Pluralsight data

---
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -0,0 +1,33 @@
+package com.pluralsight.courseinfo.cli;
+
+import com.pluralsight.courseinfo.cli.service.CourseRetrievalService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CourseRetriever {
+
+    private static final Logger LOG= LoggerFactory.getLogger(CourseRetriever.class);
+    public static void main(String[] args) {
+        LOG.info("CourseRetriever starting!");
+        if(args.length==0){
+            LOG.warn("Please provide an author name as argument");
+            return;
+        }
+        
+        try {
+            retrieveCourses(args[0]);
+        }catch (Exception e){
+            LOG.error("Unexpected Error",e);
+        }
+    }
+
+    private static void retrieveCourses(String authorId) {
+
+        LOG.info("Retrieving courses for author '{}'",authorId);
+        CourseRetrievalService courseRetrievalService=new CourseRetrievalService();
+
+        String courseToStore= courseRetrievalService.getCourseFor(authorId);
+
+        LOG.info("Retrieved the following courses {}",courseToStore);
+    }
+}
