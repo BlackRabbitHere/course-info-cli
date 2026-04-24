deb3aee802e4509ee25a0b78ef59e76b0c2113df
Kshitij <kshitijkumar514@gmail.com>
2026-04-24 12:34:27 +0530

    feat: Added the Record to retrieve specific parts of the json and also doing the json binding using jackson

--- src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -1,8 +1,12 @@
 package com.pluralsight.courseinfo.cli;
 
 import com.pluralsight.courseinfo.cli.service.CourseRetrievalService;
+import com.pluralsight.courseinfo.cli.service.PluralsightCourse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.function.Predicate;
 
 public class CourseRetriever {
 
@@ -16,6 +20,8 @@ public class CourseRetriever {
         
         try {
             retrieveCourses(args[0]);
+//            PluralsightCourse course=new PluralsightCourse("id","title","00:54:36","https://url",false);
+//            LOG.info("course: {}",course);
         }catch (Exception e){
             LOG.error("Unexpected Error",e);
         }
@@ -26,8 +32,11 @@ public class CourseRetriever {
         LOG.info("Retrieving courses for author '{}'",authorId);
         CourseRetrievalService courseRetrievalService=new CourseRetrievalService();
 
-        String courseToStore= courseRetrievalService.getCourseFor(authorId);
+        List<PluralsightCourse> courseToStore= courseRetrievalService.getCourseFor(authorId)
+                .stream()
+                .filter(Predicate.not(PluralsightCourse::isRetired))//course->!course.isRetired()
+                .toList();
 
-        LOG.info("Retrieved the following courses {}",courseToStore);
+        LOG.info("Retrieved the following {} courses {}",courseToStore.size(),courseToStore);
     }
 }
